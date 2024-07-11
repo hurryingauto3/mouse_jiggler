6fdd9690f50b101d8f0034445baacd991b49457e
Ali Hamza <alihamza19999@gmail.com>
2024-07-11 04:04:27 -0500

    ver=1.0.2 added with pypi setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mouse_jiggler",
-    version="1.0.0",
+    name="mouse_jiggler_advanced",
+    version="1.0.2",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
